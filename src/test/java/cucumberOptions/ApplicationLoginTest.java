5f7651b4cfcec6347b5e2f646d1750feeeaf6eff
devonmcbain <devon.mcbain@gmail.com>
2024-06-10 15:07:50 +0200

    Comitted

---
+++ src/test/java/cucumberOptions/ApplicationLoginTest.java
@@ -0,0 +1,31 @@
+/*
+    Testing Orange HRM Open Source Frontend user interface
+    File Name: ApplicationLoginTest.java
+    Author: Devon McBain-Charles
+    Description: BDD Framework using Cucumber/Junit test runner.
+    This allows feature files to be executed using the stepDefinition package as
+    the glue
+*/
+
+package cucumberOptions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features/loginScenarioOutline.feature"}, // specify where the feature files are kept
+        glue = {"stepDefinition"}, // specify where the steps definition files are kept
+        plugin = {"pretty", "html:src/test/java/reports/HTMLReports/Report1.html",
+                "json:src/test/java/reports/JSONReports/Report2.json",
+                "junit:src/test/java/reports/JUnitReports/Reports3.xml"}, // specify what kind of report to generate
+        dryRun = false
+        //strict = false,
+        //monochrome = false
+        //tags = "@EasyLogin"
+        //name = {"Search"}
+)
+
+public class ApplicationLoginTest {
+}
